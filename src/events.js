4cf8e9b694fe10ce5bc79a5da0e77eb3ea02f118
hamdi <hamdi.babdelhafidh@gmail.com>
2022-11-10 15:51:40 +0100

    add events data "example"

---
+++ src/events.js
@@ -0,0 +1,33 @@
+export const events = [
+  {
+    id: 1,
+    start: "15:00", // The event starts at 03:00 pm
+    duration: 60 // The duration is expressed in minutes
+  },
+
+  {
+    id: 2,
+    start: "15:20", // The event starts at 03:00 pm
+    duration: 60 // The duration is expressed in minutes
+  },
+  {
+    id: 3,
+    start: "15:30", // The event starts at 03:00 pm
+    duration: 80 // The duration is expressed in minutes
+  },
+  {
+    id: 4,
+    start: "19:30", // The event starts at 03:00 pm
+    duration: 20 // The duration is expressed in minutes
+  },
+  {
+    id: 5,
+    start: "22:50", // The event starts at 03:00 pm
+    duration: 30 // The duration is expressed in minutes
+  },
+  {
+    id: 6,
+    start: "22:60", // The event starts at 03:00 pm
+    duration: 50 // The duration is expressed in minutes
+  }
+];
